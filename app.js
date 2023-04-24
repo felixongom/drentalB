f09002942772e32cb26770735d7b2c99508ee982
felix ongom <you@example.com>
2023-04-23 21:58:15 -0900

    db correction

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ require('dotenv').config()
 const DB_NETWORK ='mongodb+srv://felixz:317111@cluster0.ujknwfv.mongodb.net/?retryWrites=true&w=majority'
 const DB_LOCAL = process.env.DB
 
-mongoose.connect(DB_LOCAL, (err, res)=>{
+mongoose.connect(DB_NETWORK, (err, res)=>{
     if(err){console.log(err)}
 
     console.log('connected');
